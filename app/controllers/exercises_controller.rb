501e7359eca3505370d91b2e4cb832a2b50789b7
Yuko0413 <soc13soc0422@gmail.com>
2024-06-11 08:53:04 +0900

    アクティブレコード課題初回

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -15,18 +15,14 @@ class ExercisesController < ApplicationController
     # 【要件】配達先の一番多い住所を返すこと
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-    @address = Address.joins(:orders).select('addresses.*,COUNT(orders.id) AS orders_count')
-    .group('addresses.id').order('orders_count DESC').limit(1).first
+    @address = .joins(:orders).select('addresses.*, COUNT(orders.id) AS orders_count').group('addresses.id').order('orders_count DESC').limit(1).first
+    #Address.joins(:orders).group(:city)select('addresses.*,COUNT(orders.id) AS orders_count').group('addresses.id').order('orders_count DESC').limit(1).first
   end
 
   def exercise4 
     # 【要件】一番お金を使っている顧客を返すこと
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
-    @customer = Customer.joins(:orders).select('customers.*, SUM(orders.total_amount) AS total_spnet')
-    .group('customers.id')
-    .order('totale_spent DESC')
-    .limit(1)
-    .first
+    @customer =  Customer.joins(orders: { order_foods: :food }).select('customers.*, SUM(foods.price) AS total_spent').group('customers.id').order('total_spent DESC').limit(1).first
   end
 end
